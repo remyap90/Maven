4e1169efb884f66e64af3efe59e0563e2d5a44cd
Remya P <Remya P@LAPTOP-GKK5FGOO>
2023-11-01 19:54:32 +0530

    excel read

---
+++ src/main/java/com/obsquara/maventraining/ExcelRead.java
@@ -0,0 +1,31 @@
+package com.obsquara.maventraining;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelRead {
+public static FileInputStream f;
+public static XSSFWorkbook wb;
+public static XSSFSheet sh;
+public static String readStringData(int i, int j) throws IOException{
+f= new FileInputStream("D:\\demo\\employee.xlsx");
+wb =new XSSFWorkbook(f);
+sh =wb.getSheet("new Sheet");
+XSSFRow r =sh.getRow(i);
+Cell c = r.getCell(j);
+return c.getStringCellValue();
+}
+public static String readNumericData(int i, int j) throws IOException{
+	f= new FileInputStream("D:\\demo\\employee.xlsx");
+	wb =new XSSFWorkbook(f);
+	sh =wb.getSheet("new Sheet");
+	XSSFRow r =sh.getRow(i);
+	Cell c = r.getCell(j);
+	return c.getStringCellValue();
+}
+}
